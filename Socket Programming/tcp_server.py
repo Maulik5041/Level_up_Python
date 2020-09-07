46543daf0f6f12b0c2601fe16e81c40742fea808
maulik_padh <maulik.padh@monsanto.com>
2020-09-07 16:47:13 -0500

    Update Socket Programming/tcp_server.py

---
+++ Socket Programming/tcp_server.py
@@ -0,0 +1,60 @@
+"""TCP server setup"""
+
+
+import argparse
+import socket
+
+
+def server(port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind(('127.0.0.1', port))
+    sock.listen(1)
+    print(f'Listening at {sock.getsockname()}')
+
+    while True:
+        print(f'Waiting for a new connection')
+        sc, sockname = sock.accept()
+        print(f'Connection from {sockname}')
+        print(f' Socket name: {sc.getsockname()}')
+        print(f' Socket peer: {sc.getpeername()}')
+        message = recvall(sc, 16)
+        print(f' message from client: {repr(message)}')
+        sc.sendall(b'Goodbye, client!')
+        sc.close()
+        print(' Closing socket')
+
+
+def recvall(sock, length):
+    data = b''
+    while len(data) < length:
+        more = sock.recv(length - len(data))
+        if not more:
+            raise EOFError('was expecting %d bytes but only received'
+                           ' %d bytes before the socket closed'
+                           % (length, len(data)))
+        data += more
+
+    return data
+
+
+def client(port):
+    host = '127.0.0.1'
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((host, port))
+    print(f'Client has been assigned the socket: {sock.getsockname()}')
+    sock.sendall(b'Greetings, server')
+    reply = recvall(sock, 16)
+    print(f'Server: {repr(reply)}')
+    sock.close()
+
+
+if __name__ == '__main__':
+    choices = {'client': client, 'server': server}
+    parser = argparse.ArgumentParser(description='Send and receive over TCP')
+    parser.add_argument('role', choices=choices, help='which role to play')
+    parser.add_argument('-p', metavar='PORT', type=int,
+                        default=3000, help='TCP port (default 1060)')
+    args = parser.parse_args()
+    function = choices[args.role]
+    function(args.p)
