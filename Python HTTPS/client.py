cfb9f5c446b333e157d1e1b3b9b86c33f65fe51f
maulik_padh <maulik.padh@monsanto.com>
2020-09-09 22:18:21 -0500

    Update Python HTTPS/client.py

---
+++ Python HTTPS/client.py
@@ -0,0 +1,12 @@
+import os
+import requests
+
+
+def get_secret_message():
+    response = requests.get("http://localhost:5683",
+                            verify="ca-public-key.pem")
+    print(f"The secret message is {response.text}")
+
+
+if __name__ == "__main__":
+    get_secret_message()
